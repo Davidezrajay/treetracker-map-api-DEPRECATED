cc27fec9126f5f27735250a4d18eefee6736c790
Matthew Xi <matthew.fullstackgis@gmail.com>
2018-10-23 16:40:42 -0700

    Implemented optimized cluster cache zone

--- index.js
+++ index.js
@@ -26,13 +26,17 @@ app.get('/trees', function (req, res) {
   let join = '';
   let joinCriteria = '';
   let filter = '';
+  let subset = false;
   if (token) {
     join = "INNER JOIN certificates ON trees.certificate_id = certificates.id AND certificates.token = '" + token + "'";
+    subset = true;
   } else if(organization) {
     join = ", certificates, donors, organizations";
     joinCriteria = "AND trees.certificate_id = certificates.id AND certificates.donor_id = donors.id AND donors.organization_id = organizations.id AND organizations.id = " + organization;
+    subset = true;
   } else if(treeid) {
     filter = 'AND trees.id = ' + treeid + ' '
+    subset = true;
   }
 
   let bounds = req.query['bounds'];
@@ -45,23 +49,46 @@ app.get('/trees', function (req, res) {
   console.log(clusterRadius);
   var sql, query
   if (clusterRadius <= 0.001) {
+
     sql = "SELECT 'point' AS type, trees.*, users.first_name as first_name, users.last_name as last_name, users.image_url as user_image_url FROM trees INNER JOIN users ON users.id = trees.user_id " + join + " WHERE active = true " + boundingBoxQuery + filter + joinCriteria;
     query = {
       text: sql
     }
+
   }
   else {
-    sql = `SELECT 'cluster'                                                   AS type, 
-                    St_asgeojson(St_centroid(clustered_locations))                 centroid, 
-                    --St_asgeojson(St_minimumboundingcircle(clustered_locations))    circle, 
-                    St_numgeometries(clustered_locations)                          count 
-             FROM   ( 
-                        SELECT Unnest(St_clusterwithin(estimated_geometric_location, $1)) clustered_locations
-                        FROM   trees ` + join + ` 
-                        WHERE  active = true ` + boundingBoxQuery + filter + joinCriteria + ` ) clusters`;
-    query = {
-      text: sql,
-      values: [clusterRadius]
+
+    // check if query is in the cached zone
+    boundingBox = bounds.split(',');
+    //37.920688261865735,-2.5769945571374615,36.365024199365735,-4.088867604371135
+    if(boundingBox[0]  <= 37.920688261865735 
+      && boundingBox[1] <= -2.5769945571374615
+      && boundingBox[2] >= 36.365024199365735
+      && boundingBox[3] >= -4.088867604371135){
+
+      sql = `SELECT 'cluster' as type,
+             St_asgeojson(location) centroid, count 
+             FROM clusters
+             WHERE zoom_level = ` + req.query['zoom_level'] + ' AND location && ST_MakeEnvelope(' + bounds + ', 4326) ';
+      query = {
+        text: sql
+      }
+      console.log(query);
+
+    } else {
+
+      sql = `SELECT 'cluster'                                                   AS type, 
+        St_asgeojson(St_centroid(clustered_locations))                 centroid, 
+        --St_asgeojson(St_minimumboundingcircle(clustered_locations))    circle, 
+        St_numgeometries(clustered_locations)                          count 
+      FROM   ( 
+        SELECT Unnest(St_clusterwithin(estimated_geometric_location, $1)) clustered_locations
+        FROM   trees ` + join + ` 
+        WHERE  active = true ` + boundingBoxQuery + filter + joinCriteria + ` ) clusters`;
+      query = {
+        text: sql,
+        values: [clusterRadius]
+      }
     }
   }
 
